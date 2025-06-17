5a69d7db099f7f5d20461175944ae203276ca0c2
Vivek <vivekbaddi2@gmail.com>
2025-06-17 13:06:27 +0530

    added / endpoint in server.js

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from frontend/public
+app.use(express.static(path.join(__dirname, "frontend/public")));
+
+// Serve frontend index.html on root "/"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/index.html"));
+});
+
 app.post("/upload", upload.single("file"), async (req, res) => {
   const { language } = req.body;
   const audioPath = req.file.path;
